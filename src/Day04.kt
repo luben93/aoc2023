094493156408d746efed586ef69755b41f743809
lucas persson <lucas.persson@konsult.atg.se>
2023-12-08 11:19:32 +0100

    day 4.5

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,9 +2,16 @@ import kotlin.math.max
 import kotlin.math.min
 
 fun main() {
-
+    val card = Regex("Card \\d: ")
     fun part1(input: List<String>): Int {
-        val rows = emptyList<Int>()
+        val rows = input.mapNotNull { row ->
+            val (winning,marks) = row.split(card).last().split("|")
+            val winningList = winning.split(" ")
+            val marksList = marks.split(" ")
+            val winningMarks = marksList.filter { winningList.contains(it) }.mapNotNull { it.takeUnless { it.isBlank() }?.toInt() }.count()
+            IntRange(1,winningMarks).reduceOrNull{ acc,_ -> acc*2 }
+        }
+        println(rows)
         return rows.sum()
     }
 
@@ -13,7 +20,7 @@ fun main() {
        return 0
     }
 
-    check(part1(readInput("Day04_test")) == 312)
+    check(part1(readInput("Day04_test")) == 13)
 //    check(part1(readInput("Day02_test2")) == 30+11+12)
 
 //    check(part2(readInput("Day02_test2")) == 281+91)
